8edb538aedb3d877a278206c1100ebce2cfd2cf4
Jonathan <1980846456@qq.com>
2023-01-08 11:25:57 +0800

    二分搜索第一次提交

---
+++ common-algorithm/src/main/java/com/djn/algorithm/array_/BinarySearch.java
@@ -0,0 +1,75 @@
+package com.djn.algorithm.array_;
+
+/**
+ * !二分查找
+ * 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target，
+ * 写一个函数搜索nums中的target，
+ * 如果目标值存在返回下标，否则返回 -1
+ */
+public class BinarySearch {
+    /**
+     * 根据二分搜索寻找对应元素下标, [left,right]闭区间
+     * 初始位置left = 0, right = length - 1
+     * 若找不到目标值，循环终止条件left > right
+     * @param numbs 一个有序且无重复元素的数组，因为一旦有重复元素，使用二分查找法返回的元素下标可能不是唯一的
+     * @param target 要查找的目标值
+     * @return 目标值的索引，若找不到目标值返回-1
+     */
+    public static int closedIntervalBinarySearch(int[] numbs, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < numbs[0] || target > numbs[numbs.length - 1]) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = numbs.length - 1; // 定义target在左闭右闭区间里
+        int middle;
+
+        while(left <= right) {
+             middle = (left + right) / 2;
+             if(numbs[middle] == target) {
+                 return middle; // 查找成功返回index
+             }else if(numbs[middle] < target) {
+                 left = middle + 1; // 查找右区间
+             }else {
+                 right = middle - 1;// 查找左区间
+             }
+        }
+
+        // 没有找到就返回-1
+        return -1;
+    }
+
+    /**
+     * 根据二分搜索寻找对应元素下标, [left,right)开区间
+     * 初始位置left = 0, right
+     * 若找不到目标值，终止条件left == right
+     * @param numbs 一个有序且无重复元素的数组，因为一旦有重复元素，使用二分查找法返回的元素下标可能不是唯一的
+     * @param target 要查找的目标值
+     * @return 目标值的索引，若找不到目标值返回-1
+     */
+    public static int openIntervalBinarySearch(int[] numbs, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < numbs[0] || target > numbs[numbs.length - 1]) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = numbs.length; // 定义target在左闭右闭区间里
+        int middle;
+        // 边界条件
+        while(left <= right) {
+             middle = (left + right) / 2;
+             if(numbs[middle] == target) {
+                 return middle; // 查找成功返回index
+             }else if(numbs[middle] < target) {
+                 left = middle + 1; // 查找右区间
+             }else {
+                 right = middle;// 查找左区间
+             }
+        }
+
+        // 没有找到就返回-1
+        return -1;
+    }
+}
---
+++ common-algorithm/src/test/java/com/djn/algorithm/array_/BinarySearchTest.java
@@ -0,0 +1,34 @@
+package com.djn.algorithm.array_;
+
+import com.djn.algorithm.array_.BinarySearch;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class BinarySearchTest {
+    @Test
+    public void testClosedIntervalSearchResult() {
+        // input
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        // trueOutput:the index of 9 is 4
+        int trueResult = 4;
+
+        int result = BinarySearch.closedIntervalBinarySearch(nums, target);
+        assertEquals(result, trueResult);
+    }
+
+    @Test
+    public void testOpenIntervalSearchResult() {
+        // input
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        // trueOutput:the index of 9 is 4
+        int trueResult = 4;
+
+        int result = BinarySearch.openIntervalBinarySearch(nums, target);
+        assertEquals(result, trueResult);
+    }
+}
